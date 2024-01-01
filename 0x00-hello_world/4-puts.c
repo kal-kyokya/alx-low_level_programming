db6f7fa1d8cfca424d3eefd533caa3858dd5e49e
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-01-01 00:42:08 -0800

    Print a string using the puts function, try 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- *main: Entry point to the function.
+ *main - Entry point to the function.
  *
  *Return: 0 if successfully run through.
  */
-int main()
+int main(void)
 {
-	char str[]= "\"Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
-	return 0;
+	return (0);
 }
