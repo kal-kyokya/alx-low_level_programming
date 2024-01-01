f916194df65ed996762fc1125cca623c25bf9688
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2023-12-31 23:13:59 -0800

    Jan 1st, Wouh back baby, run only the preprocessor.

---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+This directory is the first to be created in this repository and will hold all hold all files related to the Hello world project.
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main: Entry point to the program
+ *
+ *Return: Always 0 (Success)
+ */
+int main()
+{
+	return 0;
+}
