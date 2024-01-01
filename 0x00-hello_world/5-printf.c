497c0071636b07b56821924b837516fd905996fc
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-01-01 00:50:24 -0800

    Print string using the print function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point to the function
+ *
+ *Return: 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
