fe9ae010f5e61e6cb0e9e41ca08f97df746d0d52
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-01-02 01:54:49 -0800

    Print all single digit numbers from base 10.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point to the program.
+ *
+ *Return: 0 if program successfully runs.
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		printf("%d", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
