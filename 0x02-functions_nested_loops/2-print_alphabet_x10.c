524e4c3fa2f213a776c7ff02dbe514fcdd89462c
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-01-04 00:18:25 -0800

    Print all lowercases on 10 lines.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_alphabet - Print all 26 lowercases on 10 lines.
+ *
+ *Return: None.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int x;
+
+	c = 'a';
+	x = 0;
+	while (x <= 9)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		c = 'a';
+		x++;
+	}
+
+}
