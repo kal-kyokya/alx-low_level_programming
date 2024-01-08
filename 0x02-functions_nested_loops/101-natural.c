8f8dde0d48f86fe0374328a8ea40376cf4e6f099
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-01-08 09:44:03 -0800

    Sum all multiples of 3 and 5 under 1024try, 1.

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,13 +9,19 @@ int main(void)
 {
 	int x1, x2, counter;
 
-	counter = 0;
+	counter = x1 = x2 = 0;
 	while (counter < 1024)
 	{
 		if (counter % 3 == 0)
+		{
 			x1 += counter;
+			printf("  1024mod3 = %d--", counter);
+		}
 		else if (counter % 5 == 0)
+		{
 			x2 += counter;
+			printf("  1024mod5 = %d--", counter);
+		}
 		counter++;
 	}
 	counter = x1 + x2;
