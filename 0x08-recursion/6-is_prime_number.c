2ce9d5d01929bf775a47a9ca2c1a28a59aa6d73c
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-01-12 12:07:44 -0800

    Evaluate a number as prime or not.

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int helper_function(int input, int counter);
+
 /**
  *is_prime_number - Tells whether a number is prime or not.
  *@x: Number to be assessed.
@@ -8,10 +10,25 @@
  */
 int is_prime_number(int x)
 {
-	int result;
-
 	if (x <= 1)
-		result = 0;
-	
-	return (result);
+		return (0);
+	else if (x == 2 || x == 3 || x == 5)
+		return (1);
+	else if ((x % 2 == 0) || (x % 3 == 0) || (x % 5 == 0))
+		return (0);
+	return (helper_function(x, 1));
+}
+
+/**
+ *helper_function - Tests if input is only divsible by itself.
+ *@input: Number to be evaluated.
+ *@counter: Test every digit lesser than input.
+ *
+ *Return: Either the prime number or 0 if unavailable.
+ */
+int helper_function(int input, int counter)
+{
+  if (((input / counter) == 1) && ((input % counter) == 0))
+		return (1);
+	return (helper_function(input, counter + 1));
 }
