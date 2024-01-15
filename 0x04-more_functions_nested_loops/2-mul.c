2c6eb100f3b7625e1c506f9f4f1cd3bfb431bb1d
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-01-15 11:41:00 -0800

    Compute product of two inputs.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Computes the product of two integer inputs.
+ * @x: First input.
+ * @y: Second input.
+ *
+ * Return: Product of the two inputs.
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
