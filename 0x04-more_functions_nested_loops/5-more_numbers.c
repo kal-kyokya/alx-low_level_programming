36cb0fe19cfcfe84463762d262f21b6c29769965
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-01-19 11:37:42 -0800

    Print numbers 0 to 14 on 10 lines.

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print 10 lines of numbers from 0 to 14.
+ *
+ * Return: Nothing.
+ */
+void more_numbers(void)
+{
+	int counter1, counter2;
+
+	counter1 = 0;
+	while (counter1 < 10)
+	{
+		counter2 = 0;
+		while (counter2 <= 14)
+		{
+			if (counter2 > 9)
+			{
+				_putchar((counter2 / 10) + '0');
+				_putchar((counter2 % 10) + '0');
+			}
+			else
+				_putchar(counter2 + '0');
+			counter2++;
+		}
+		_putchar('\n');
+		counter1++;
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int _isdigit(int x);
 int mul(int x, int y);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 
 
 #endif
