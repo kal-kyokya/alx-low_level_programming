5c3627256a687c87d7c8f815024908ca51a19add
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-01-21 01:46:12 -0800

    Use hash sign to draw square on screen.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - Using hash signs print a square on screen.
+ * @size: Number of hash signs to found on sides.
+ *
+ * Return: Nothing.
+ */
+void print_square(int size)
+{
+	int counter1, counter2;
+
+	counter1 = counter2 = 0;
+	if (size == 0)
+		_putchar('\n');
+	else
+	{
+		while (counter1 < size)
+		{
+			while (counter2 < size)
+			{
+				_putchar('#');
+				counter2++;
+			}
+			counter2 = 0;
+			_putchar('\n');
+			counter1++;
+		}
+	}
+}
