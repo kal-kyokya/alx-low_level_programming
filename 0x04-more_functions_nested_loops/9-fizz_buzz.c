8c26cfeea885833fe445dddc290ea94ccfbf9195
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-01-21 02:25:52 -0800

    Execute the "Fizz-Buzz test", try 3.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,7 +17,7 @@ int main(void)
 		else if (counter % 3 == 0)
 			printf("Fizz ");
 		else if (counter % 5 == 0)
-				printf("Buzz ");
+			printf("Buzz ");
 		else
 			printf("%d ", counter);
 		counter++;
