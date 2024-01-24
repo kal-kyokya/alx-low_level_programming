5f3be6caf7f5f4b1cf0c770c781aaad5b0618b0a
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-01-24 09:13:30 -0800

    Initialize a variable of structure foo data type.

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initializes a variable of type struct dog.
+ * @name: Name of the dog.
+ * @owner: Owner of the dog.
+ * @age: Age of the dog.
+ * @d: Name of initialized variable.
+ *
+ * Return: Nothing.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *bond;
+
+	bond = d;
+	bond->name = name;
+	bond->owner = owner;
+	bond->age = age;
+}
