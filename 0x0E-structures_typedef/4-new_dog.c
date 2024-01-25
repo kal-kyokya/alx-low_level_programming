e715b6071df05a8bbd5a253a001ceac9b56546e7
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-01-25 08:31:07 -0800

    Create a variable using function that uses typedef name, try 1.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,19 +7,43 @@
  * @age: Age of the dog.
  * @owner: Owner of the dog.
  *
- * Return: Pointer to Newly created Variable of type dog_t
+ * Return: Pointer to newly created Variable of type dog_t
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *bond;
+	dog_t *dog2;
+	int sl_n, sl_o, counter1, counter2;
+	char *cp_n, *cp_o;
 
-	bond = malloc(sizeof(dog_t));
-	if (bond == NULL)
+	sl_n = sl_o = counter1 = counter2 = 0;
+	while (*(name + sl_n) != '\0')
+		sl_n++;
+	while (*(owner + sl_o) != '\0')
+		sl_o++;
+	dog2 = malloc(sizeof(dog_t));
+	if (dog2 == NULL)
 		return (NULL);
-	bond->name = name;
-	bond->age = age;
-	bond->owner = owner;
+	if (name == NULL || owner == NULL)
+	{
+		free(dog2);
+		return (NULL);
+	}
+	cp_n = malloc(sl_n);
+	cp_o = malloc(sl_o);
+	while (counter1	< sl_n)
+	{
+		*(cp_n + counter1) = *(name + counter1);
+		counter1++;
+	}
+	while (counter2	< sl_o)
+	{
+		*(cp_o + counter2) = *(owner + counter2);
+		counter2++;
+	}
+	dog2->name = name;
+	(*dog2).owner = owner;
+	dog2->age = age;
 
-	return (bond);
+	return (dog2);
 }
