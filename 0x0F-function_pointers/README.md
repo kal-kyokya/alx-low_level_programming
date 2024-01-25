dc4329c29f107570a4fa437ac2066cfd833f09ae
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-01-25 11:49:09 -0800

    Create a function that takes a pointer to function argument.

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This directory will be home to all files required for me to complete the 0x0F ALX project; Function pointers.
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef F_POINTER
+#define F_POINTER
+
+void print_name(char *name1, void (*function)(char *name2));
+int _putchar(char c);
+
+
+#endif
