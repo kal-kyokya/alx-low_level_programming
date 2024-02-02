b3e231fd810fe93d9ab348ca8a8dbd7c592e8993
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-02-02 05:02:53 -0800

    Convert first encountered number in string to an int.

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts first encountered number inside string to type int.
+ * @s: String to be assessed.
+ *
+ * Return: An integer.
+ */
+int _atoi(char *s)
+{
+	int len, count1, count2, result;
+
+	len = count1 = count2 = 0;
+	while (s[len] != '\0')
+		len++;
+	while (count1 < len)
+	{
+		if (s[count1] >= '0' && s[count1] <= '9')
+		{
+			result = (s[count1] - '0');
+			while (s[count1 + 1] >= '0' && s[count1 + 1] <= '9')
+			{
+				result = (result * 10) + (s[count1 + 1] - '0');
+				count1++;
+			}
+			break;
+		}
+		else if (count1 == (len - 1))
+			result = 0;
+		count1++;
+	}
+	while (count2 < len)
+	{
+		if (s[count2] == '-')
+			result *= (-1);
+		else if (s[count2] >= '0' && s[count2] <= '9')
+			break;
+		count2++;
+	}
+
+	return (result);
+}
