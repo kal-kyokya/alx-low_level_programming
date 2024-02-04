a01613a436475705a9e568127f7cc2e192d77fe6
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-02-04 12:13:47 -0800

    Pass char to n string elements.

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory will be home to all files needed for completion of the 0x07 alx low level programming project.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_memset(char *s, char c, unsigned int x);
+
+
+#endif
