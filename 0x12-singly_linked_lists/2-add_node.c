7ab5f5af0f05b612f25bfc3d61f8dd627bc9f2ca
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-03-22 13:37:32 -0700

    Add nodes at beginning of linked list, try 1.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,7 +3,7 @@
 /**
  * add_note - Set a node as header in linked list.
  * @head: Pointer to the first linked list node.
- * @str: String value of added node.
+ * @s: String value of added node.
  *
  * Return: A pointer to the first node.
  */
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *s)
 	if (node == NULL)
 	{
 		fprintf(stderr, "Memory allocation failed.\n");
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	node->len = strlen(s);
 	node->str = strdup(s);
