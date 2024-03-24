e37231b8fc1b928aa6b781f78360293582b23042
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-03-24 15:07:01 -0700

    Deal with assembly programming in C, try 2.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 10, 0  ; 10 is the ASCII code for a new line character
-
-section .text
+global main
     extern printf
-    global main
-
 main:
-    push rbp
-    mov rdi, hello
-    call printf
-    pop rbp
-
-    ; Exit the program
-    mov rax, 60       ; syscall number for exit
-    xor rdi, rdi      ; exit status (0)
-    syscall
+	mov edi, format
+	xor eax,eax
+	call printf
+	mov eax, 0
+	ret
+format: db `Hello, Holberton\n`,0
