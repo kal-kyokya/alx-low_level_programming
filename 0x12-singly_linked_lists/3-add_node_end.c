a03e2ede2d45bb042b4a047e00b98a637d69c12a
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-03-24 07:27:17 -0700

    Add node at the ned of linked list.

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a node at the end of a linked list.
+ * @h: Pointer allowed access to out of scope pointer head.
+ * @str: String to be passed as value in added node.
+ *
+ * Return: A pointer to the first linked list node.
+ */
+list_t *add_node_end(list_t **h, const char *str)
+{
+	list_t *node, *helper;
+
+	node = malloc(sizeof(list_t));
+	node->str = strdup(str);
+	if (node->str == NULL)
+		free(node->str);
+	node->len = strlen(str);
+	node->next = NULL;
+	if (*h == NULL)
+		*h = node;
+	else
+	{
+		helper = *h;
+		while (helper->next != NULL)
+			helper = helper->next;
+		helper->next = node;
+	}
+
+	return (*h);
+}
