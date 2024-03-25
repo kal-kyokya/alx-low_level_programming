adb88ac30b07ca8246e1f70b67f3b92d0d1f1f92
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-03-25 06:22:32 -0700

    Free all malloced memory blocks, try 2.

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,11 +9,13 @@
 void free_listint(listint_t *head)
 {
 	listint_t *node;
+	listint_t *next_node;
 
 	node = head;
 	while (node != NULL)
 	{
+		next_node = node->next;
 		free(node);
-		node = node->next;
+		node = next_node;
 	}
 }
