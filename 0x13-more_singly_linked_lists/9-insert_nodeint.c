97a58c5acfc3af736b9b7592594cf26131076970
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-03-25 09:05:57 -0700

    Add a node at index n position, try 2.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int num)
 	new_node->n = num;
 	if (idx == 0)
 	{
-		new_node = node;
+		new_node->next = node;
 		*head = new_node;
 		return (new_node);
 	}
