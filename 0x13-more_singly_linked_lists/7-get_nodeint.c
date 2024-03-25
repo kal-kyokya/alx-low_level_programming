61ddef1f2e5f1c31f95b3d0be762689fdd93d305
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-03-25 08:02:05 -0700

    Print the nth node's int value.

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Locates a node given its index value.
+ * @head: Pointer to the head pointer.
+ * @index: Ordinal value locating a specific elementwithin the struc.
+ *
+ * Return: A pointer to the node at index n.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node;
+	unsigned int count;
+
+	if (head == NULL)
+		return (NULL);
+	count = 0;
+	node = head;
+	if (index == 0)
+		return (node);
+	else
+	{
+		while (count != index)
+		{
+			node = node->next;
+			count++;
+		}
+	}
+
+	return (node);
+}
