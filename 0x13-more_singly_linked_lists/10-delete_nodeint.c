05751dd1d2bd10e17cd75d6466252fec903bc1b3
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-03-25 13:58:38 -0700

    Delete node at index n, try 1.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,6 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = node->next;
 		node->next = NULL;
+		free(node);
 		return (1);
 	}
 	while (count != index)
@@ -33,6 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	node->next = helper->next;
 	helper->next = NULL;
+	free(helper);
 
 	return (1);
 }
