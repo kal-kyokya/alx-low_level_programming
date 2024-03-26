08c9a1aa18e9e8ba99cb579c4eed0e08e6df91b0
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-03-25 18:31:40 -0700

    Task 1.

--- 0x18-dynamic_libraries/dynamic.c
+++ 0x18-dynamic_libraries/dynamic.c
@@ -1,102 +1,77 @@
 #include "dynamic.h"
-#include <stddef.h>
 
-int _putchar(char c)
-{
-
+int my_abs(int n) {
+    // Implementation of _abs function
 }
 
-int _islower(int c)
-{
-
+int my_atoi(char *s) {
+    // Implementation of _atoi function
 }
 
-int _isalpha(int c)
-{
-
+int my_isalpha(int c) {
+    // Implementation of _isalpha function
 }
 
-int _abs(int n)
-{
-
+int my_isdigit(int c) {
+    // Implementation of _isdigit function
 }
 
-int _isupper(int c)
-{
-
+int my_islower(int c) {
+    // Implementation of _islower function
 }
 
-int _isdigit(int c)
-{
-
+int my_isupper(int c) {
+    // Implementation of _isupper function
 }
 
-int _strlen(char *s)
-{
-
+int my_memcpy(char *dest, char *src, unsigned int n) {
+    // Implementation of _memcpy function
 }
 
-void _puts(char *s)
-{
-
+int my_memset(char *s, char b, unsigned int n) {
+    // Implementation of _memset function
 }
 
-char *_strcpy(char *dest, char *src)
-{
-
+int my_putchar(char c) {
+    // Implementation of _putchar function
 }
 
-int _atoi(char *s)
-{
-
+void my_puts(char *s) {
+    // Implementation of _puts function
 }
 
-char *_strcat(char *dest, char *src)
-{
-
+char *my_strcat(char *dest, char *src) {
+    // Implementation of _strcat function
 }
 
-char *_strncat(char *dest, char *src, int n)
-{
-
+char *my_strchr(char *s, char c) {
+    // Implementation of _strchr function
 }
 
-char *_strncpy(char *dest, char *src, int n)
-{
-
+int my_strcmp(char *s1, char *s2) {
+    // Implementation of _strcmp function
 }
 
-int _strcmp(char *s1, char *s2)
-{
-
+char *my_strcpy(char *dest, char *src) {
+    // Implementation of _strcpy function
 }
 
-char *_memset(char *s, char b, unsigned int n)
-{
-
+char *my_strncat(char *dest, char *src, int n) {
+    // Implementation of _strncat function
 }
 
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-
+char *my_strncpy(char *dest, char *src, int n) {
+    // Implementation of _strncpy function
 }
 
-char *_strchr(char *s, char c)
-{
-
+unsigned int my_strspn(char *s, char *accept) {
+    // Implementation of _strspn function
 }
 
-unsigned int _strspn(char *s, char *accept)
-{
-
+char *my_strpbrk(char *s, char *accept) {
+    // Implementation of _strpbrk function
 }
 
-char *_strpbrk(char *s, char *accept)
-{
-
-}
-
-char *_strstr(char *haystack, char *needle)
-{
-
+char *my_strstr(char *haystack, char *needle) {
+    // Implementation of _strstr function
 }
