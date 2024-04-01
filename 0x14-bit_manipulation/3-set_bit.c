829945f14b383e91badfa236c77170eec3d50166
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-04-01 04:09:04 -0700

    Insert a 1 at nth index of number.

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - Sets the nth bit to 1.
+ * @n: Pointer to the number being edited.
+ * @index: Index at which insertion occurs.
+ *
+ * Return: 1 if it worked, -1 otherwise.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	*n = *n | (1 << index);
+
+	return (1);
+}
