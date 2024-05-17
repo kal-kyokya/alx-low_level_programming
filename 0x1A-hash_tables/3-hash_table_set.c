cf64910f72f14bdfafd0c367d56d679dc9e6d3f8
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-17 11:56:35 -0700

    Insert element x in Hash table, try 1.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -48,7 +48,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 		ht->array[index] = node;
-	else if(strcmp(ht->array[index]->key, key_copy) == 0)
+	else if (strcmp(ht->array[index]->key, key_copy) == 0)
 		ht->array[index]->key = key_copy;
 	else
 	{
