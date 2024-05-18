d223c0a52fd7a17f2219c1ae1ae0e1ff1e2e2756
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-17 18:38:49 -0700

    Task 100.

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints data in order they appear in array of hash table.
+ * @ht: Hash table to be printed.
+ *
+ * Return: Nothing.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+
+}
