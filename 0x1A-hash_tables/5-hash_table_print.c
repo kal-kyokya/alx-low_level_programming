546fed1648edd98929bea0b4e6407637c9c940d5
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-18 03:30:37 -0700

    Print hash table.

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,5 +8,31 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int index;
+	hash_node_t *node;
+	int print_flag;
 
+	if (ht == NULL)
+		return;
+
+	index = 0;
+	print_flag = 0;
+
+	printf("{");
+	while (index < ht->size)
+	{
+		node = ht->array[index];
+
+		while (node != NULL)
+		{
+			if (print_flag)
+		  		printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			print_flag = 1;
+			node = node->next;
+		}
+
+		index++;
+	}
+	printf("}\n");
 }
--- 0x1A-hash_tables/5-main.c
+++ 0x1A-hash_tables/5-main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+#define ARRAY_SIZE 1024
+
 /**
  * main - check the code
  *
@@ -11,8 +13,9 @@
 int main(void)
 {
 	hash_table_t *ht;
+	unsigned long int index;
 
-	ht = hash_table_create(1024);
+	ht = hash_table_create(ARRAY_SIZE);
 
 	hash_table_print(ht);
 
@@ -25,6 +28,21 @@ int main(void)
 	hash_table_set(ht, "98", "Battery Street");
 
 	hash_table_print(ht);
+
+	index = key_index((const unsigned char *)"c", ARRAY_SIZE);
+	printf("\nc: --%lu--", index);
+	index = key_index((const unsigned char *)"python", ARRAY_SIZE);
+	printf("\npython: --%lu--", index);
+	index = key_index((const unsigned char *)"Bob", ARRAY_SIZE);
+	printf("\nBob: --%lu--", index);
+	index = key_index((const unsigned char *)"N", ARRAY_SIZE);
+	printf("\nN--%lu--", index);
+	index = key_index((const unsigned char *)"Asterix", ARRAY_SIZE);
+	printf("\nAsterix: --%lu--", index);
+	index = key_index((const unsigned char *)"Betty", ARRAY_SIZE);
+	printf("\nBetty: --%lu--", index);
+	index = key_index((const unsigned char *)"98", ARRAY_SIZE);
+	printf("\n98: --%lu--\n", index);
 
 	return (EXIT_SUCCESS);
 }
