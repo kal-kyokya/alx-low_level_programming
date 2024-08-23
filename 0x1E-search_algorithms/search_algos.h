d34953500415ee9e1392eca76f03da9128106fe4
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-08-23 04:26:13 -0700

    Implement Linear search algorithm.

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,9 +6,9 @@
 #include <stdlib.h>
 
 /**
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
+ * n: Integer
+ * index: Index of the node in the list
+ * next: Pointer to the next node
  *
  * Description: singly linked list node structure
  */
@@ -22,10 +22,10 @@ typedef struct listint_s
 /**
  * struct skiplist_s - Singly linked list with an express lane
  *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
+ * n: Integer
+ * index: Index of the node in the list
+ * next: Pointer to the next node
+ * express: Pointer to the next node in the express lane
  *
  * Description: singly linked list node structure with an express lane
  */
